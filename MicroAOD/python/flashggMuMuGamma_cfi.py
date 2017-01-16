d6fa2e61e7ea1b3253fa18e9dc90ac15a2ed8fec
abeschi <a.beschi@campus.unimib.it>
2017-01-16 16:15:05 +0100

    MuMuGammaDumper under development

--- MicroAOD/python/flashggMuMuGamma_cfi.py
+++ MicroAOD/python/flashggMuMuGamma_cfi.py
@@ -4,7 +4,7 @@ import FWCore.ParameterSet.Config as cms
 
 flashggMuMuGamma = cms.EDProducer('FlashggMuMuGammaProducer',
                                   DiMuonTag=cms.InputTag('flashggDiMuons'),
-                                  PhotonTag=cms.InputTag('flashggPhotons'),
+                                  PhotonTag=cms.InputTag('PhotonWithUpdatedIdMVA'),
                                   VertexTag=cms.InputTag('offlineSlimmedPrimaryVertices'),
                                   ##Parameters                                                
                                   minPhotonPT=cms.double(10.)
