d5bb2988da11f289a0be7fce4612b383fd9d22e7
abeschi <a.beschi@campus.unimib.it>
2017-06-05 17:27:23 +0200

    Minor updates

--- Taggers/test/zeeDumper_cfg.py
+++ Taggers/test/zeeDumper_cfg.py
@@ -29,9 +29,9 @@ process.source = cms.Source("PoolSource",
                             fileNames=cms.untracked.vstring(      
         #data                    
         #"/store/group/phys_higgs/cmshgg/ferriff/flashgg/RunIISpring16DR80X-2_3_0-25ns_Moriond17_MiniAODv2/2_3_0/DoubleEG/RunIISpring16DR80X-2_3_0-25ns_Moriond17_MiniAODv2-2_3_0-v0-Run2016E-23Sep2016-v1/161114_163114/0000/myMicroAODOutputFile_817.root"
- 	"/store/group/phys_higgs/cmshgg/sethzenz/flashgg/ReMiniAOD-03Feb2017-2_5_0-test/2_5_0/SingleElectron/ReMiniAOD-03Feb2017-2_5_0-test-2_5_0-v0-Run2016G-03Feb2017-v1/170211_080819/0000/myMicroAODOutputFile_674.root"
+ 	#"/store/group/phys_higgs/cmshgg/sethzenz/flashgg/ReMiniAOD-03Feb2017-2_5_0-test/2_5_0/SingleElectron/ReMiniAOD-03Feb2017-2_5_0-test-2_5_0-v0-Run2016G-03Feb2017-v1/170211_080819/0000/myMicroAODOutputFile_674.root"
         # mc
-        #"/store/group/phys_higgs/cmshgg/sethzenz/flashgg/RunIISummer16-2_4_0-25ns_Moriond17/2_4_0/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-madgraphMLM-herwigpp_30M/RunIISummer16-2_4_0-25ns_Moriond17-2_4_0-v0-RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/161225_213425/0000/myMicroAODOutputFile_1.root"
+        "/store/group/phys_higgs/cmshgg/sethzenz/flashgg/RunIISummer16-2_4_0-25ns_Moriond17/2_4_0/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-madgraphMLM-herwigpp_30M/RunIISummer16-2_4_0-25ns_Moriond17-2_4_0-v0-RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/161225_213425/0000/myMicroAODOutputFile_1.root"
         #"/store/group/phys_higgs/cmshgg/sethzenz/flashgg/RunIISummer16-2_4_1-25ns_Moriond17/2_4_1/DYJetsToEE_M-50_LTbinned_95To100_5f_LO_13TeV-madgraph_pythia8/RunIISummer16-2_4_1-25ns_Moriond17-2_4_1-v0-RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/170118_090503/0000/myMicroAODOutputFile_5.root"
         #"/store/group/phys_higgs/cmshgg/sethzenz/flashgg/RunIISummer16-2_4_1-25ns_Moriond17/2_4_1/DYToEE_NNPDF30_13TeV-powheg-pythia8/RunIISummer16-2_4_1-25ns_Moriond17-2_4_1-v0-RunIISummer16MiniAODv2-EGM0_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/170121_195541/0000/myMicroAODOutputFile_9.root"
         ))
@@ -282,6 +282,7 @@ cfgTools.addCategories(process.diphotonDumper,
 process.load('RecoMET.METFilters.eeBadScFilter_cfi')
 process.eeBadScFilter.EERecHitSource = cms.InputTag("reducedEgamma","reducedEERecHits") # Saved MicroAOD Collection (data only) 
 process.dataRequirements = cms.Sequence()
+
 if customize.processId == 'Data':
     process.dataRequirements += process.eeBadScFilter
     process.hltHighLevel= hltHighLevel.clone(HLTPaths = cms.vstring(
